362597353b449275a3e53e911926029274ca224a
Oliwier K <o.krawczyk06@gmail.com>
2026-06-06 18:55:12 +0200

    feat: add profile settings

--- game/urls.py
+++ game/urls.py
@@ -9,5 +9,7 @@ path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='game/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
+    path('profile/', views.profile_settings, name='profile'),
+
     path('', views.register, name='home'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -14,3 +16,23 @@ def register(request):
 
  
     return render(request, 'game/register.html', {'form': form})
+
+@login_required
+def profile_settings(request):
+    if request.method == "POST":
+
+        if 'update_profile' in request.POST:
+
+            form = UserUpdateForm(request.POST, instance=request.user)
+            if form.is_valid():
+                form.save()
+                return redirect('profile')
+            
+        elif 'delete_account' in request.POST:
+            user = request.user
+            logout(request)
+            user.delete()
+            return redirect('register')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'game/profile.html', {'form': form})
