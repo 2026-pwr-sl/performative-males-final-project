9a39c7ac5dde9f31bb373c1891f8a43b737dc1d2
Oliwier K <o.krawczyk06@gmail.com>
2026-06-06 17:24:02 +0200

    feat: user login

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     path('register/', views.register, name='register'),
-     path('', views.register,name='home' )
+path('register/', views.register, name='register'),
+    
+    # builtin 
+    path('login/', auth_views.LoginView.as_view(template_name='game/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    path('', views.register, name='home'),
 ]
